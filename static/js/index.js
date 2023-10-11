62a98ae11f61bc77f041e8152b197fd0d0b87084
rrahman <rashidur92@hotmail.com>
2023-10-11 16:40:05 +0100

    testing queries

--- static/js/index.js
+++ static/js/index.js
@@ -7,62 +7,88 @@
 
 // addDiv()
 
-async function processLogin(form) {
+async function processLogin(form, query) {
   let username = form.username.value;
   let password = form.password.value;
-
   let jwtRequestUrl = "https://learn.01founders.co/api/auth/signin";
-
   let encodedAuth = btoa(username + ":" + password);
-  console.log(encodedAuth);
 
   let jwt = await fetch(jwtRequestUrl, {
     method: "POST",
     headers: {
       Authorization: "Basic " + encodedAuth,
       "Content-Type": "application/json",
-    }
+    },
   })
     .then(function (response) {
       if (response.ok) {
-        console.log("got a response");
         return response.json();
       }
-      console.log("response failed", response);
       throw response;
     })
-    .catch((response) => response);
-  console.log("jwt is: ", jwt);
-  
-  let graphqlquery =  {query: `query {
-    user {
-      id
-      login
-    }
-  }`}
-  
-  console.log("body after restringifying: ",  JSON.stringify(graphqlquery))
-  let dataRequestUrl = "https://learn.01founders.co/api/graphql-engine/v1/graphql"
+    .catch((response) => console.log("unable to retrieve jwt: ", response));
+  return jwt;
+}
+
+async function graphqlRequest(jwt, query) {
+  let dataRequestUrl =
+    "https://learn.01founders.co/api/graphql-engine/v1/graphql";
   let data = await fetch(dataRequestUrl, {
     method: "POST",
     headers: {
-      Authorization: "Bearer "+jwt,
+      Authorization: "Bearer " + jwt,
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(graphqlquery)
-  }).then(function(response){
-    if (response.ok){
-      return response.json()
-    }
-    throw response
-  }).catch(response => console.log(response))
-  console.log(data)
+    body: JSON.stringify(query),
+  })
+    .then(function (response) {
+      if (response.ok) {
+        return response.json();
+      }
+      throw response;
+    })
+    .catch((response) => console.log(response));
   return data;
+}
+
+async function displayData(form) {
+  requestedData = {
+    query: `query {
+      user {
+        id
+      }
+      xp: transaction_aggregate(
+        where: {type: {_eq: "xp"}, eventId: {_eq: "134"}}
+      ) {
+        aggregate {
+          sum {
+            amount
+          }
+        }
+      }
+    }`,
+
+  };
+
+
+  // {"data":{"user":[{"id":3345,"login":"rrahman"}]}}
+  // {"type":"data","id":"g8yum40n5x","payload":{"data":{"xp":{"aggregate" : {"sum" : {"amount" : 570575.0}}},"level":[{"amount":24.0}]}}}
+  let jwt = await processLogin(form, requestedData);
+  console.log(jwt);
+  let data = await graphqlRequest(jwt, requestedData);
+  console.log(data);
+  let resultsDiv = document.getElementById("results");
+  resultsDiv.innerText = JSON.stringify(data);
+  for (let key in data.data){
+    let newdiv = document.createElement("div")
+    newdiv.innerText = "results for " + key + ": " + JSON.stringify(data.data[key])
+    resultsDiv.appendChild(newdiv)
+  }
 }
 
 let form = document.getElementById("loginForm");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
-  processLogin(form);
+  await displayData(form);
 });
