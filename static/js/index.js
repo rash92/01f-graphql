65e7cbd5da6606a6ce906a64a1594ce8742d8898
rrahman <rashidur92@hotmail.com>
2023-10-12 18:29:10 +0100

    figuring out more complicated queries

--- static/js/index.js
+++ static/js/index.js
@@ -7,7 +7,7 @@
 
 // addDiv()
 
-async function processLogin(form, query) {
+async function processLogin(form) {
   let username = form.username.value;
   let password = form.password.value;
   let jwtRequestUrl = "https://learn.01founders.co/api/auth/signin";
@@ -43,52 +43,130 @@ async function graphqlRequest(jwt, query) {
   })
     .then(function (response) {
       if (response.ok) {
+        console.log("response ok: ", response)
         return response.json();
       }
+      console.log("graphql request failed: ", response)
       throw response;
     })
-    .catch((response) => console.log(response));
-  return data;
+    .catch((response) => response);
+    console.log(data)
+  return data.data;
 }
 
-async function displayData(form) {
-  requestedData = {
-    query: `query {
-      user {
-        id
-      }
-      xp: transaction_aggregate(
-        where: {type: {_eq: "xp"}, eventId: {_eq: "134"}}
-      ) {
-        aggregate {
-          sum {
-            amount
-          }
-        }
-      }
-    }`,
-
-  };
 
 
-  // {"data":{"user":[{"id":3345,"login":"rrahman"}]}}
-  // {"type":"data","id":"g8yum40n5x","payload":{"data":{"xp":{"aggregate" : {"sum" : {"amount" : 570575.0}}},"level":[{"amount":24.0}]}}}
-  let jwt = await processLogin(form, requestedData);
-  console.log(jwt);
+async function displayData(form, requestedData) {
+  let jwt = await processLogin(form);
   let data = await graphqlRequest(jwt, requestedData);
-  console.log(data);
+  console.log("data after graphql request: ", data)
+  
   let resultsDiv = document.getElementById("results");
-  resultsDiv.innerText = JSON.stringify(data);
-  for (let key in data.data){
-    let newdiv = document.createElement("div")
-    newdiv.innerText = "results for " + key + ": " + JSON.stringify(data.data[key])
-    resultsDiv.appendChild(newdiv)
+  if (!data){
+    let errorDiv = document.createElement("div")
+    errorDiv.innerText = "Unfortunately the request failed, please check your credentials or contact the administrator"
+    resultsDiv.appendChild(errorDiv)
+    return
   }
+  addDataToDiv(resultsDiv, data)
 }
 
 let form = document.getElementById("loginForm");
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
-  await displayData(form);
+  await doStuff(form);
 });
+
+async function doStuff(form){
+  let lastMonthQuery = xpQueryStringBeforeDate(dateMonthsBeforeNow(1))
+  console.log(lastMonthQuery)
+  let query = {
+    query: `query ($lang: jsonb){
+      user {
+        id
+        githubId
+        login
+        discordId
+        discordLogin
+        profile
+        attrs
+        campus
+      }
+      transaction {
+        id
+        type
+        amount
+        userId
+        attrs
+        createdAt
+        path
+        objectId
+        eventId
+      }
+      xp_per_major_project: transaction(where:{type:{_eq:"xp"}, object:{type:{_eq:"project"}}}){
+        amount
+        object{
+          id
+          name
+          
+        }
+      }
+      Go_projects_soloable: object(where: {type: {_eq: "project"}, attrs: {_contains: $lang}}) {
+        id
+        name
+        attrs
+        childrenAttrs
+      }
+    }`,
+    variables: {
+      "lang": {"language" : "Go", "groupMin": 1}
+    }
+  };
+  await displayData(form, query)
+  return false
+}
+
+async function addDataToDiv(div, data){
+  console.log("raw data being added to div: ",data);
+  for (let key in data) {
+    let newdiv = document.createElement("div");
+    newdiv.innerText = "results for " + key + ":"
+    if (Array.isArray(data[key])) {
+      let listDiv = document.createElement("ul");
+      for (let elem in data[key]) {
+        let listElem = document.createElement("li");
+        listElem.innerText =
+          "item " + elem + ": " + JSON.stringify(data[key][elem]);
+        listDiv.appendChild(listElem);
+      }
+      newdiv.appendChild(listDiv);
+    } else if (typeof data[key] === "object"){
+      addDataToDiv(newdiv, data[key])
+    }else {
+      let miscDiv = document.createElement("div")
+      miscDiv.innerText = JSON.stringify(data[key])
+      newdiv.appendChild(miscDiv)
+    }
+    div.appendChild(newdiv);
+  }
+}
+
+function dateMonthsBeforeNow(months){
+  let timeNow = new Date(Date.now())
+  timeNow.setMonth(timeNow.getMonth() - months)
+  return timeNow
+}
+
+function xpQueryStringBeforeDate(date){
+ let queryString = ` xp_before_date: transaction_aggregate(
+    where: {type: {_eq: "xp"}, eventId: {_eq: "134"}, createdAt: {_lt: "`+date.getMonth() + "1" + +`"}}
+  ) {
+    aggregate {
+      sum {
+        amount
+      }
+    }
+  }`
+  return queryString
+}
