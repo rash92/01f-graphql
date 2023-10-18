36336aff533c4fc161c8eb2a8093b20d954b1844
rrahman <rashidur92@hotmail.com>
2023-10-18 15:13:13 +0100

    functions to find xp between two dates

--- static/js/index.js
+++ static/js/index.js
@@ -43,32 +43,32 @@ async function graphqlRequest(jwt, query) {
   })
     .then(function (response) {
       if (response.ok) {
-        console.log("response ok: ", response)
+        console.log("response ok: ", response);
         return response.json();
       }
-      console.log("graphql request failed: ", response)
+      console.log("graphql request failed: ", response);
       throw response;
     })
     .catch((response) => response);
-    console.log(data)
+  console.log(data);
   return data.data;
 }
-
-
 
 async function displayData(form, requestedData) {
   let jwt = await processLogin(form);
   let data = await graphqlRequest(jwt, requestedData);
-  console.log("data after graphql request: ", data)
-  
+  console.log("data after graphql request: ", data);
+
   let resultsDiv = document.getElementById("results");
-  if (!data){
-    let errorDiv = document.createElement("div")
-    errorDiv.innerText = "Unfortunately the request failed, please check your credentials or contact the administrator"
-    resultsDiv.appendChild(errorDiv)
-    return
+  if (!data) {
+    let errorDiv = document.createElement("div");
+    errorDiv.innerText =
+      "Unfortunately the request failed, please check your credentials or contact the administrator";
+    resultsDiv.appendChild(errorDiv);
+    return;
   }
-  addDataToDiv(resultsDiv, data)
+  addDataToDiv(resultsDiv, data);
+  return data;
 }
 
 let form = document.getElementById("loginForm");
@@ -78,9 +78,11 @@ form.addEventListener("submit", async (e) => {
   await doStuff(form);
 });
 
-async function doStuff(form){
-  let lastMonthQuery = xpQueryStringBeforeDate(dateMonthsBeforeNow(1))
-  console.log(lastMonthQuery)
+async function doStuff(form) {
+  let lastMonthQuery = xpQueryBeforeDate("01 01 2023");
+  console.log("last month query: ", lastMonthQuery);
+  let betweenMonthsQuery = xpQueryBetweenDates("01 01 2023", "05 01 2023")
+  console.log("between months query: ", betweenMonthsQuery)
   let query = {
     query: `query ($lang: jsonb){
       user {
@@ -119,22 +121,40 @@ async function doStuff(form){
       }
     }`,
     variables: {
-      "lang": {"language" : "Go", "groupMin": 1}
-    }
+      lang: { language: "Go", groupMin: 1 },
+    },
   };
-  await displayData(form, query)
-  
-  let pieChart = createPieChart(100, [5,5,10,20, 30, 50], ["adsadasda","bdsadad","cdsadsa","ddsadsa","edsad", "fdsad"])
-  document.getElementById("svgs").appendChild(pieChart)
-  document.getElementById("svgs").appendChild(createLineGraph("","time","xp","","",200,200,[[0,0],[10,10],[10,20],[20,25],[50,50],[100,100],[150,200]]))
-  return false
+  let data = await displayData(form, query);
+  console.log("data returned from displayData: ", data);
+  let monthdata = await displayData(form, lastMonthQuery);
+  console.log("month data: ", monthdata);
+  let betweenMonthsData = await displayData(form, betweenMonthsQuery)
+  console.log("between months data: ", betweenMonthsData)
+  let pieChart = createPieChart(
+    100,
+    [5, 5, 10, 20, 30, 50],
+    ["adsadasda", "bdsadad", "cdsadsa", "ddsadsa", "edsad", "fdsad"]
+  );
+  document.getElementById("svgs").appendChild(pieChart);
+  document.getElementById("svgs").appendChild(
+    createLineGraph("", "time", "xp", "", "", 200, 200, [
+      [0, 0],
+      [10, 10],
+      [10, 20],
+      [20, 25],
+      [50, 50],
+      [100, 100],
+      [150, 200],
+    ])
+  );
+  return false;
 }
 
-async function addDataToDiv(div, data){
-  console.log("raw data being added to div: ",data);
+async function addDataToDiv(div, data) {
+  console.log("raw data being added to div: ", data);
   for (let key in data) {
     let newdiv = document.createElement("div");
-    newdiv.innerText = "results for " + key + ":"
+    newdiv.innerText = "results for " + key + ":";
     if (Array.isArray(data[key])) {
       let listDiv = document.createElement("ul");
       for (let elem in data[key]) {
@@ -144,131 +164,234 @@ async function addDataToDiv(div, data){
         listDiv.appendChild(listElem);
       }
       newdiv.appendChild(listDiv);
-    } else if (typeof data[key] === "object"){
-      addDataToDiv(newdiv, data[key])
-    }else {
-      let miscDiv = document.createElement("div")
-      miscDiv.innerText = JSON.stringify(data[key])
-      newdiv.appendChild(miscDiv)
+    } else if (typeof data[key] === "object") {
+      addDataToDiv(newdiv, data[key]);
+    } else {
+      let miscDiv = document.createElement("div");
+      miscDiv.innerText = JSON.stringify(data[key]);
+      newdiv.appendChild(miscDiv);
     }
     div.appendChild(newdiv);
   }
 }
 
-function dateMonthsBeforeNow(months){
-  let timeNow = new Date(Date.now())
-  timeNow.setMonth(timeNow.getMonth() - months)
-  return timeNow
+function dateMonthsBeforeNow(months) {
+  let timeNow = new Date(Date.now());
+  timeNow.setMonth(timeNow.getMonth() - months);
+  return timeNow;
 }
 
-function xpQueryStringBeforeDate(date){
- let queryString = ` xp_before_date: transaction_aggregate(
-    where: {type: {_eq: "xp"}, eventId: {_eq: "134"}, createdAt: {_lt: "`+date.getMonth() + "1" + +`"}}
+function xpQueryBeforeDate(date) {
+  let queryString = {
+    query:
+      `{
+  xp_before_date: transaction_aggregate(
+    where: {type: {_eq: "xp"}, eventId: {_eq: "134"}, createdAt: {_lt: "` +
+      date +
+      `"}}
   ) {
     aggregate {
       sum {
         amount
       }
     }
-  }`
-  return queryString
+  }
+}`,
+  };
+  return queryString;
 }
 
-function createPieChart(radius, values, labels){
-  let total = values.reduce((runningTotal, elem) => runningTotal+elem)
-  let pieChart = document.createElementNS("http://www.w3.org/2000/svg", "svg")
-  pieChart.setAttribute("width", 2*radius)
-  pieChart.setAttribute("height", 2*radius + 15*values.length)
-  let keyElem = document.createElementNS("http://www.w3.org/2000/svg","g")
-  let currentAngle = 0
-  for (let index in values){
+function xpQueryBetweenDates(startDate, endDate){
+  let queryString = {
+    query:
+      `{
+  xp_before_date: transaction_aggregate(
+    where: {type: {_eq: "xp"}, eventId: {_eq: "134"}, createdAt: {_lt: "` +
+    endDate +
+      `"}, _and: {createdAt: {_gt: "`+startDate+`"}}}
+  ) {
+    aggregate {
+      sum {
+        amount
+      }
+    }
+  }
+}`,
+  };
+  return queryString;
+}
+
+function createPieChart(radius, values, labels) {
+  let total = values.reduce((runningTotal, elem) => runningTotal + elem);
+  let pieChart = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  pieChart.setAttribute("width", 2 * radius);
+  pieChart.setAttribute("height", 2 * radius + 15 * values.length);
+  let keyElem = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  let currentAngle = 0;
+  for (let index in values) {
     //main pie chart
-    value = values[index]
-    let proportion = value/total
-    let angle = proportion * 2*Math.PI
-    let colourGrayscale = "rgb(" + (currentAngle*255/Math.PI)+", "+ (currentAngle*255/Math.PI) + ", "+(currentAngle*255/Math.PI)+")"
-    let colour = "rgb(" + (255-currentAngle*255/Math.PI) + ", "+(0)+", "+ (currentAngle*255/Math.PI)+")"
-    pieChart.appendChild(createSegmentPath(radius, currentAngle, angle, colour))
+    value = values[index];
+    let proportion = value / total;
+    let angle = proportion * 2 * Math.PI;
+    let colourGrayscale =
+      "rgb(" +
+      (currentAngle * 255) / Math.PI +
+      ", " +
+      (currentAngle * 255) / Math.PI +
+      ", " +
+      (currentAngle * 255) / Math.PI +
+      ")";
+    let colour =
+      "rgb(" +
+      (255 - (currentAngle * 255) / Math.PI) +
+      ", " +
+      0 +
+      ", " +
+      (currentAngle * 255) / Math.PI +
+      ")";
+    pieChart.appendChild(
+      createSegmentPath(radius, currentAngle, angle, colour)
+    );
     // pieChart.appendChild(createSegmentPath(radius, currentAngle, angle, colourGrayscale))
 
     //keys
-    let key = document.createElementNS("http://www.w3.org/2000/svg","g")
-    key.setAttribute("color",colour)
-    
-    let keySquare = document.createElementNS("http://www.w3.org/2000/svg","rect")
-    keySquare.setAttribute("width","10")
-    keySquare.setAttribute("height","10")
-    keySquare.setAttribute("style", "fill:"+colour)
-    keySquare.setAttribute("y", 2*radius + 15*index)
-    let keyLabel = document.createElementNS("http://www.w3.org/2000/svg","text")
-    keyLabel.innerHTML = labels[index]
-    keyLabel.setAttribute("y", 2*radius + 10+ 15*(index))
-    keyLabel.setAttribute("x", 15)
-    key.appendChild(keySquare)
-    key.appendChild(keyLabel)
-    keyElem.appendChild(key)
-    currentAngle += angle
+    let key = document.createElementNS("http://www.w3.org/2000/svg", "g");
+    key.setAttribute("color", colour);
+
+    let keySquare = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "rect"
+    );
+    keySquare.setAttribute("width", "10");
+    keySquare.setAttribute("height", "10");
+    keySquare.setAttribute("style", "fill:" + colour);
+    keySquare.setAttribute("y", 2 * radius + 15 * index);
+    let keyLabel = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "text"
+    );
+    keyLabel.innerHTML = labels[index];
+    keyLabel.setAttribute("y", 2 * radius + 10 + 15 * index);
+    keyLabel.setAttribute("x", 15);
+    key.appendChild(keySquare);
+    key.appendChild(keyLabel);
+    keyElem.appendChild(key);
+    currentAngle += angle;
   }
-  pieChart.appendChild(keyElem)
-  console.log("pie chart created!")
+  pieChart.appendChild(keyElem);
+  console.log("pie chart created!");
 
-  return pieChart
+  return pieChart;
 }
 
-
-function createSegmentPath(r, startAngle, angle, colour){
-  console.log("segment created with r: ", r, " starting angle: ", startAngle, " angle: ", angle, " colour: ", colour)
-  let moveToCenter = "M " + r + ", " + r + " " 
-  let moveToEdge = "L " + (Math.cos(startAngle)*r + r) + ", " + (Math.sin(startAngle)*r + r) + " " 
-  let moveAlongArc = "A " + r + ", " + r + " 0 " + (angle<Math.PI?"0":"1") + "1" + (Math.cos(startAngle+angle)*r + r) + ", " + (Math.sin(startAngle+angle)*r + r) + " "
-  let moveBack = "Z"
-  let pathString = moveToCenter + moveToEdge + moveAlongArc + moveBack
-  let path = document.createElementNS('http://www.w3.org/2000/svg', 'path')
-  path.setAttribute("d", pathString)
-  path.setAttribute("fill", colour)
-  path.setAttribute("stroke", "red")
-  return path
+function createSegmentPath(r, startAngle, angle, colour) {
+  console.log(
+    "segment created with r: ",
+    r,
+    " starting angle: ",
+    startAngle,
+    " angle: ",
+    angle,
+    " colour: ",
+    colour
+  );
+  let moveToCenter = "M " + r + ", " + r + " ";
+  let moveToEdge =
+    "L " +
+    (Math.cos(startAngle) * r + r) +
+    ", " +
+    (Math.sin(startAngle) * r + r) +
+    " ";
+  let moveAlongArc =
+    "A " +
+    r +
+    ", " +
+    r +
+    " 0 " +
+    (angle < Math.PI ? "0" : "1") +
+    "1" +
+    (Math.cos(startAngle + angle) * r + r) +
+    ", " +
+    (Math.sin(startAngle + angle) * r + r) +
+    " ";
+  let moveBack = "Z";
+  let pathString = moveToCenter + moveToEdge + moveAlongArc + moveBack;
+  let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+  path.setAttribute("d", pathString);
+  path.setAttribute("fill", colour);
+  path.setAttribute("stroke", "red");
+  return path;
 }
 
-function createLineGraph(title, xlabel, ylabel, xaxis, yaxis, width, height, points){
-  let origin = [50,50+height]
-  let lineGraphSvg = document.createElementNS('http://www.w3.org/2000/svg', "svg")
-  lineGraphSvg.setAttribute("width", 1.5*width)
-  lineGraphSvg.setAttribute("height", 1.5*height)
-  let xaxisElem = document.createElementNS('http://www.w3.org/2000/svg',"line")
-  xaxisElem.setAttribute("x1", origin[0])
-  xaxisElem.setAttribute("x2", origin[0]+width)
-  xaxisElem.setAttribute("y1", origin[1])
-  xaxisElem.setAttribute("y2", origin[1])
-  xaxisElem.setAttribute("stroke", "red")
-  let yaxisElem = document.createElementNS('http://www.w3.org/2000/svg',"line")
-  yaxisElem.setAttribute("x1", origin[0])
-  yaxisElem.setAttribute("x2", origin[0])
-  yaxisElem.setAttribute("y1", origin[1])
-  yaxisElem.setAttribute("y2", origin[1]-height)
-  yaxisElem.setAttribute("stroke", "blue")
-  lineGraphSvg.appendChild(xaxisElem)
-  lineGraphSvg.appendChild(yaxisElem)
-  let xlabelElem = document.createElementNS('http://www.w3.org/2000/svg',"text")
-  xlabelElem.setAttribute("x", width/2)
-  xlabelElem.setAttribute("y", 75+height)
-  xlabelElem.innerHTML = xlabel
-  lineGraphSvg.appendChild(xlabelElem)
-  let ylabelElem = document.createElementNS('http://www.w3.org/2000/svg',"text")
-  ylabelElem.setAttribute("x", 0)
-  ylabelElem.setAttribute("y", height/2)
-  ylabelElem.innerHTML = ylabel
-  lineGraphSvg.appendChild(ylabelElem)
+function createLineGraph(
+  title,
+  xlabel,
+  ylabel,
+  xaxis,
+  yaxis,
+  width,
+  height,
+  points
+) {
+  let origin = [50, 50 + height];
+  let lineGraphSvg = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "svg"
+  );
+  lineGraphSvg.setAttribute("width", 1.5 * width);
+  lineGraphSvg.setAttribute("height", 1.5 * height);
+  let xaxisElem = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "line"
+  );
+  xaxisElem.setAttribute("x1", origin[0]);
+  xaxisElem.setAttribute("x2", origin[0] + width);
+  xaxisElem.setAttribute("y1", origin[1]);
+  xaxisElem.setAttribute("y2", origin[1]);
+  xaxisElem.setAttribute("stroke", "red");
+  let yaxisElem = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "line"
+  );
+  yaxisElem.setAttribute("x1", origin[0]);
+  yaxisElem.setAttribute("x2", origin[0]);
+  yaxisElem.setAttribute("y1", origin[1]);
+  yaxisElem.setAttribute("y2", origin[1] - height);
+  yaxisElem.setAttribute("stroke", "blue");
+  lineGraphSvg.appendChild(xaxisElem);
+  lineGraphSvg.appendChild(yaxisElem);
+  let xlabelElem = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "text"
+  );
+  xlabelElem.setAttribute("x", width / 2);
+  xlabelElem.setAttribute("y", 75 + height);
+  xlabelElem.innerHTML = xlabel;
+  lineGraphSvg.appendChild(xlabelElem);
+  let ylabelElem = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "text"
+  );
+  ylabelElem.setAttribute("x", 0);
+  ylabelElem.setAttribute("y", height / 2);
+  ylabelElem.innerHTML = ylabel;
+  lineGraphSvg.appendChild(ylabelElem);
 
-  let lineElem = document.createElementNS("http://www.w3.org/2000/svg", "polyline")
-  let pointsStr = ""
-  for (let point in points){
-    pointsStr += " " + (origin[0] + points[point][0]) + "," + (origin[1] - points[point][1])
+  let lineElem = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "polyline"
+  );
+  let pointsStr = "";
+  for (let point in points) {
+    pointsStr +=
+      " " +
+      (origin[0] + points[point][0]) +
+      "," +
+      (origin[1] - points[point][1]);
   }
-  lineElem.setAttribute("points", pointsStr)
-  lineElem.setAttribute("stroke", "black")
-  lineElem.setAttribute("fill", "none")
-  lineGraphSvg.appendChild(lineElem)
-  return lineGraphSvg
+  lineElem.setAttribute("points", pointsStr);
+  lineElem.setAttribute("stroke", "black");
+  lineElem.setAttribute("fill", "none");
+  lineGraphSvg.appendChild(lineElem);
+  return lineGraphSvg;
 }
-
